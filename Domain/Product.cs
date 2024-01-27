f673d9cbf27bcdebf5fec5c9927b130e22c65e3a
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2024-01-26 23:35:09 -0300

    Criando construtor na entidade product

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -4,7 +4,16 @@ public sealed class Product : EntityID
 {
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public string PurchasePrice { get; private set; }
-    public string SellingPrice { get; private set; }
-    public string StokeQuantity { get; private set; }
+    public decimal PurchasePrice { get; private set; }
+    public decimal SellingPrice { get; private set; }
+    public int StokeQuantity { get; private set; }
+
+    public Product(string name, string description, decimal purchasePrice, decimal sellingPrice, int stokeQuantity)
+    {
+        Name = name;
+        Description = description;
+        PurchasePrice = purchasePrice;
+        SellingPrice = sellingPrice;
+        StokeQuantity = stokeQuantity;
+    }
 }
