b9c902c0d4fae1f6aafd40fb63622be1cc3f1f99
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2024-01-26 22:01:49 -0300

    Criando interfaces em domain
    Conteúdo: Criando interface para product e um geral generico com todas as assinaturas para add, update, remove e removeRange

---
+++ Domain/Interfaces/IGeneralRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Database;
+
+internal interface IGeneralRepository
+{
+    void Add<T>(T entity) where T : class;
+    void Update<T>(T entity) where T : class;
+    void Delete<T>(T entity) where T : class;
+    void DeleteRange<T>(T entity) where T : class;
+
+    Task<bool> SaveChangesAsync();
+}
---
+++ Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Interfaces;
+
+public interface IProductRepository
+{
+    //IEnumerable eh mais pratico e seguro quando quer apenas iterar sobre a colecao de elementos
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetProductByIdAsync(int? id);
+}
