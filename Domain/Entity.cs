74867a5559083cb3b9dc0bfa2eb102aa6bcc1e5d
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2024-01-26 21:13:12 -0300

    Criando entidade de produto e entity
    Conteúdo: Foi criado uma entidade de produto e outro chamada Entity que contém configurações gerais para id e guid; Foi configurado as properties para usar http por enquanto

---
+++ Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+public abstract class Entity
+{
+
+}
+public abstract class EntityID : Entity
+{
+    public int ID { get; private set; }
+}
+
+public abstract class EntityGUID : Entity
+{
+    public Guid GUID { get; private set; }
+}
+
---
+++ Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+
+public sealed class Product : EntityID
+{
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string PurchasePrice { get; private set; }
+    public string SellingPrice { get; private set; }
+    public string StokeQuantity { get; private set; }
+}
