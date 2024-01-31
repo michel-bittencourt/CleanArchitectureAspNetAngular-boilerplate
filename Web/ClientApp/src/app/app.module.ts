01b6b282e2864909f058551137c67ba85f170cb7
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2024-01-30 21:16:05 -0300

    Criando tabela consumindo api e criando navbar
    Conteúdo: Foi criada uma tabela usando bootstrap e ngFor consumindo os dados da api de produtos; Foi criada uma navbar simples; Foi criado um componente de home com uma imagem

--- Web/ClientApp/src/app/app.module.ts
+++ Web/ClientApp/src/app/app.module.ts
@@ -1,13 +1,21 @@
+// Faz a definição de localidade permitando a formatação 1.2 de moeda brasileira ex: 1,00, 50.00, 1.000, 1.500,50
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products/products.component';
 import { SuppliersComponent } from './components/suppliers/suppliers.component';
 import { TestesComponent } from './components/testes/testes.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -15,6 +23,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ProductsComponent,
     SuppliersComponent,
     TestesComponent,
+    NavComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
