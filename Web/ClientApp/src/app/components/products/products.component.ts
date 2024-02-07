9861cb687ac839905b5433ceddf02fc797661d22
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2024-02-06 21:56:03 -0300

    Melhorando acesso a rota de api [IMPORTANTE]
    Conteúdo: Foi melhorado o modo que o subscribe é usado

--- Web/ClientApp/src/app/components/products/products.component.ts
+++ Web/ClientApp/src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -9,9 +11,10 @@ import { ProductService } from 'src/app/services/product.service';
   // providers: [ProductService],
 })
 export class ProductsComponent implements OnInit {
-  public products: any[] = [];
-  public filteredProducts: any = [];
-  image: boolean = true;
+  public products: Product[] = [];
+  public filteredProducts: Product[] = [];
+  public image: boolean = true;
+
   private _listFilter: string = '';
 
   // get eh uma palavra reservada para dizer que o metodo retorna algo
@@ -27,7 +30,7 @@ export class ProductsComponent implements OnInit {
       : this.products;
   }
 
-  public listFilteredProduct(filterBy: string): any {
+  public listFilteredProduct(filterBy: string): Product[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product) =>
       product.name.toLocaleLowerCase().includes(filterBy)
@@ -36,7 +39,7 @@ export class ProductsComponent implements OnInit {
 
   constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.getProducts();
   }
 
@@ -52,12 +55,24 @@ export class ProductsComponent implements OnInit {
   }
 
   public getProducts(): void {
-    this.productService.getProduct().subscribe(
-      (response: any) => {
-        this.products = response;
+    const observer = {
+      next: (_products: Product[]) => {
+        this.products = _products;
         this.filteredProducts = this.products;
+        console.log(this.products);
       },
-      (error: any) => console.log(error)
-    );
+      erro: (error: any) => console.log(error),
+      complete: () => {},
+    };
+    this.productService.getProduct().subscribe(observer);
+
+    // Esse forma abaixo funciona mas gera um alerta no subscribe dizendo que essa forma de usar ficara obsoleto
+    // this.productService.getProduct().subscribe(
+    //   (products: Product[]) => {
+    //     this.products = products;
+    //     this.filteredProducts = this.products;
+    //   },
+    //   (error) => console.log(error)
+    // );
   }
 }
