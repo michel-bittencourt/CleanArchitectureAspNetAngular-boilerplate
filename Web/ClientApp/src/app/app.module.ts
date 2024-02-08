a5f4fea59fb1346abb4cb09f8c6dbf66dfc4653c
michel-bittencourt <michellemos_bittencourt@hotmail.com>
2024-02-07 21:08:23 -0300

    Usando tooltip

--- Web/ClientApp/src/app/app.module.ts
+++ Web/ClientApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 // Faz a definição de localidade permitando a formatação 1.2 de moeda brasileira ex: 1,00, 50.00, 1.000, 1.500,50
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,6 +10,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/initial/home.component';
@@ -16,9 +18,9 @@ import { NavComponent } from './components/nav/nav.component';
 import { ProductsComponent } from './components/products/products.component';
 import { SuppliersComponent } from './components/suppliers/suppliers.component';
 import { TestesComponent } from './components/testes/testes.component';
-import { ProductService } from './services/product.service';
-import { DateTimeFormatPipe } from './helpers/Dates/date-time-format.pipe';
 import { DateFormatPipe } from './helpers/Dates/date-format.pipe';
+import { DateTimeFormatPipe } from './helpers/Dates/date-time-format.pipe';
+import { ProductService } from './services/product.service';
 
 registerLocaleData(localePt);
 
@@ -38,8 +40,10 @@ registerLocaleData(localePt);
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot(),
     FormsModule,
+    CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent],
